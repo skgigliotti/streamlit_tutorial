1e0b816cd6ab335612866805e9456cdcac5e90cc
sophia <sophia@sophias-MacBook-Pro.local>
2025-08-13 20:26:13 -0700

    All the changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,35 @@ import plotly.express as px
 
 df = pd.read_csv("state_data.csv")
 
-st.header("US State Demographics")
+columns = ["Total Population", "Median Household Income"]
+column = st.selectbox("Data:", columns)
 
-# Let user select which state to graph
 state = st.selectbox("State:", df["State"].unique())
 
 # Create a graph of total population
 df_state = df[df["State"] == state]
-fig = px.line(
+population_fig = px.line(
     df_state, x="Year", y="Total Population", title=f"Total Population of {state}"
 )
-st.plotly_chart(fig)
 
-# Show the entire dataframe
-st.write("All Data")
-st.dataframe(df)
+# Create a graph of Median Household Income
+df_state = df[df["State"] == state]
+
+graph_tab, table_tab = st.tabs(["Graph", "Table"])
+with graph_tab:
+    income_fig = px.line(
+        df_state, x="Year", y="Median Household Income", title=f"Median Household Income of {state}"
+    )
+
+    if column == 'Total Population':
+        st.header("🏃 Changes in US State Demographics Over Time")
+        st.plotly_chart(population_fig)
+    else:
+        st.header("💸 Median Household Income")
+        st.plotly_chart(income_fig)
+
+with table_tab:
+    st.write("All Data")
+    st.dataframe(df)
+
+
